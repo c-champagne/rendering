2d13ba9c245b843185403ba86d12166668ce70b8
c-champagne <cassielchampagne@gmail.com>
2020-03-04 23:18:01 -0500

    Completed restaurants

--- renderRestaurants.js
+++ renderRestaurants.js
@@ -2,9 +2,29 @@
 function renderRestaurants(restaurants) {
     // HINT: You can use <img /> tags that point to these playing card images: 
     // https://commons.wikimedia.org/wiki/Category:SVG_playing_cards
+    var pricePoint = "";
+    function printPrice(priceRating){
+      for (var i=1; i <= priceRating; i++) { 
+          pricePoint += "$";
+      }
+          var finalPrice = pricePoint;
+          pricePoint = "";
+           return finalPrice;
+        }
+    
+    function drawBlock(restaurant) {
+        return `
+        <div style="width: 200px; margin: 5px; padding: 5px; border-radius: 5%; background-color: lightgray; font-weight: bold;">
+            <h2 style="font-weight: bold; margin-bottom: 0px;">${restaurant.name}</h2>
+            <p style="color: gray; margin-bottom: 0px;">${restaurant.type}</p>
+            <span style="font-size: 30px; color: #55cf34;">${printPrice(restaurant.priceRating)}</span>
+        </div>
+        `
+    }
+    
     return `
-        <div class="text-center mt-5">
-            <code>${JSON.stringify(restaurants)}</code>
+    <div style="display: flex; flex-direction: row;">
+        ${restaurants.map(drawBlock).join("")}
         </div>
     `
 }
@@ -33,3 +53,7 @@ function restaurants() {
     content.innerHTML = renderRestaurants(restaurantsAbstraction);
 
 }
+
+// <div class="text-center mt-5">
+// <code>${JSON.stringify(restaurants)}</code>
+// </div>
